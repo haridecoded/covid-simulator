9e1f43f2bb043166c7902b66b1082105456a0407
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2020-04-30 13:42:12 -0400

    fix: make newly added infected people disobedient and asymptomatic

--- resources/js/main.js
+++ resources/js/main.js
@@ -1583,6 +1583,7 @@ class SimulationWorld {
             index: this.gameObjects.length,
             movingState: 'moving',
             infectedState: 'sick',
+            disobedient: true,
             x: this.canvasRight / 2,
             y: this.canvasBottom/2,
             radius: Math.max(this.canvas.width / 200, 3.6),
@@ -1591,7 +1592,7 @@ class SimulationWorld {
         });
         infectedPerson.data = this.population.people[this.gameObjects.length];
         infectedPerson.data.infected = true;
-        infectedPerson.data.symptoms = true;
+        infectedPerson.data.symptoms = false;
         this.gameObjects.push(infectedPerson);
         
     }
